05fd16e26cb5d9e817fb1b3c18a52346455ce758
NatGuz1608 <nataliaguzmanburgos@gmail.com>
2026-07-14 23:53:34 -0500

    Mensaje inicial del proyecto

---
+++ Assets/Scripts/UsernameDisplay.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UsernameDisplay : MonoBehaviour
+{
+	[SerializeField] PhotonView playerPV;
+	[SerializeField] TMP_Text text;
+
+
+	void Start()
+	{
+		if(playerPV.IsMine)
+		{
+			gameObject.SetActive(false);
+		}
+
+		text.text = playerPV.Owner.NickName;
+	}
+
+}
